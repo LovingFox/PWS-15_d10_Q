3c896ac612c472a9c0b90d1d258454f2f8335f75
Ivan Revyakin <LovingFox@GMail.com>
2020-10-08 18:52:52 +0300

    Debug off

--- vehicles/settings.py
+++ vehicles/settings.py
@@ -23,16 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'u1h4bzm+cdovkpv^&!z$3*7l&p5mt$aap+u^_+mlp7)krl2hx=')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sf.rtru.tk']
 
-DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda r: True,
-}
+#DEBUG_TOOLBAR_CONFIG = {
+#    'SHOW_TOOLBAR_CALLBACK': lambda r: True,
+#}
 
 
 # Application definition
